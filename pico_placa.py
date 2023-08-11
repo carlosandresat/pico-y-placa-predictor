25ae26c13eb2203951b889baf06b379176998d2f
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2023-08-11 09:21:45 -0500

    Encapsulate main for not running when module

--- pico_placa.py
+++ pico_placa.py
@@ -6,13 +6,17 @@ class PicoYPlacaPredictor:
         return True
     
 
-predictor = PicoYPlacaPredictor()
+def main():
+    predictor = PicoYPlacaPredictor()
     
-license_plate = input("Enter license plate number (e.g., ABC-1234): ")
-date_str = input("Enter date (YYYY-MM-DD): ")
-time_str = input("Enter time (HH:MM): ")
+    license_plate = input("Enter license plate number (e.g., ABC-1234): ")
+    date_str = input("Enter date (YYYY-MM-DD): ")
+    time_str = input("Enter time (HH:MM): ")
               
-if predictor.can_drive(license_plate, date_str, time_str):
-    print("You can drive.")
-else:
-    print("You can't drive during this time.")
+    if predictor.can_drive(license_plate, date_str, time_str):
+        print("You can drive.")
+    else:
+        print("You can't drive during this time.")
+
+if __name__ == "__main__":
+    main()
